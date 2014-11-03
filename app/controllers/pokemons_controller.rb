4c1b94b42045774f16ea00456c56a31a06fafbbd
Ingrid <ingridhong@gmail.com>
2014-11-03 01:09:44 -0800

    I am Pokemon master

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,10 +1,40 @@
 class PokemonsController < ApplicationController
 	
+	def show
+	end
+
 	def capture
 		@pokemon = Pokemon.find(params[:id])
 		@pokemon.trainer = current_trainer
 		@pokemon.save
 		redirect_to root_path
 	end
-   
+
+	def damage
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.health -=10
+		@pokemon.save
+		if @pokemon.health <= 0
+	   		@pokemon.destroy
+	   	end
+		redirect_to trainer_path(@pokemon.trainer_id)
+	end 
+
+	def new
+		@pokemon = Pokemon.new
+	end
+
+	def create 
+		@pokemon = Pokemon.create(params.require(:pokemon).permit(:name))
+		@pokemon.trainer = current_trainer
+		@pokemon.health = 100
+		@pokemon.level = 1
+		if !@pokemon.save
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+		redirect_to trainer_path(@pokemon.trainer_id)
+		end
+		
+   		
+	end
+
 end
